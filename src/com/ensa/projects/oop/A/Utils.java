093c6496abfc70e7f0ec6e61da58a8b4d2a3e2b6
sha3loul <zarhloulyasser@gmail.com>
2023-10-18 20:03:51 +0100

    feat: extending methods to generic types

--- src/com/ensa/projects/oop/A/Utils.java
+++ src/com/ensa/projects/oop/A/Utils.java
@@ -1,16 +1,52 @@
 package com.ensa.projects.oop.A;
 
 public class Utils {
-    static int add(int i, int j) {
+    public static int add(int i, int j) {
         return i + j;
     }
 
-    static int compare(int i, int j) {
+    public static int compare(int i, int j) {
         return i - j;
     }
 
-    static int compare(String s1, String s2) {
+    public static int compare(String s1, String s2) {
         if (s2.equals(s1)) return 0;
         return s2.length() - s1.length();
     }
+
+    public static <T extends Additionable<T>> T add(T i, T j) {
+        return i.add(j);
+    }
+
+    public static <T extends Comparable<T>> int compare(T i, T j) {
+        return i.compareTo(j);
+    }
+    public static <T extends Cloneable> T clone(T object) {
+        return null;
+    }
+
+    public static <T extends Stockable> T store(T object) {
+        return null;
+    }
+
+    public static <T extends Envoyable> T send(T object) {
+        return null;
+    }
+
+    public static <T extends Testable> T test(T object) {
+        return null;
+    }
+}
+
+interface Additionable<T> {
+    T add(T other);
+}
+
+interface Stockable {
+}
+
+interface Envoyable {
+}
+
+interface Testable {
 }
