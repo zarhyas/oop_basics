72f9c94e391bec4174a3cbd05b246f5c819e0e13
sha3loul <zarhloulyasser@gmail.com>
2023-10-18 20:30:27 +0100

    todo: suggest a more evolutive schema

---
+++ src/com/ensa/projects/oop/C/Etudiant.java
@@ -0,0 +1,15 @@
+package com.ensa.projects.oop.C;
+
+class Etudiant {
+
+    String nom;
+    String filiere;
+    String num;
+    String rue;
+    String ville;
+    String pays;
+    String emploi;
+    String nomEcole;
+    String villeEcole;
+}
+
