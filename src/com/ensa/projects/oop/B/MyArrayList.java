3a6addef9501c8dac2bfa364b9993b0600a48a5b
sha3loul <zarhloulyasser@gmail.com>
2023-10-18 20:13:05 +0100

    feat: implemented MyArrayList with add(Object o) and get(int index)

---
+++ src/com/ensa/projects/oop/B/MyArrayList.java
@@ -0,0 +1,30 @@
+package com.ensa.projects.oop.B;
+
+public class MyArrayList {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(Object o) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size++] = o;
+    }
+
+    public Object get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return elements[index];
+    }
+
+    private void ensureCapacity() {
+        int newSize = elements.length * 2;
+        elements = java.util.Arrays.copyOf(elements, newSize);
+    }
+}
